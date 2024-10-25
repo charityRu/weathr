2a82abf7177634c6285118f3d937487f5452479f
charity <nicolecharity22@gmail.com>
2024-10-24 18:18:32 -0800

    Added span ID for description

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement=document.querySelector("#description")
+
+
+
+
   cityElement.innerHTML = response.data.city;
+  descriptionElement.innerHTML=response.data.condition.description
   temperatureElement.innerHTML = temperature;
 }
 
